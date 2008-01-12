82f4bb0865def5e3f8ed5305bc50c6abee840160
jreese <jreese@ad4848e8-a12d-0410-9a8f-f544a64f585b>
2008-01-12 19:25:56 +0000

    Added basic default video options with validation.
    git-svn-id: svn+ssh://leetcode.net/home/jreese/backup/srv/svn/repos/rit/2dgames/trunk@444 ad4848e8-a12d-0410-9a8f-f544a64f585b

--- SnailsPace/SnailsPace/Config/VideoConfig.cs
+++ SnailsPace/SnailsPace/Config/VideoConfig.cs
@@ -11,7 +11,26 @@ namespace A_Snail_s_Pace.Config
         public VideoConfig()
             : base()
         {
+            Dictionary<String, Double> doubles = new Dictionary<string, double>();
+
+            // Default values
+            doubles.Add("height", 600);
+            doubles.Add("width", 800);
+            setDefaults(doubles);
+
+            // Load user preferences
             this.readFile(videoConfigFile);
+
+            // Validation
+            if (getDouble("height") < 480)
+            {
+                setDouble("height", 480);
+            }
+
+            if (getDouble("width") < 640)
+            {
+                setDouble("width", 640);
+            }
         }
 
         public void save()
