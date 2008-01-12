1a1aec808c6056e5a670d2ff74ab9921ce31fcd2
jreese <jreese@ad4848e8-a12d-0410-9a8f-f544a64f585b>
2008-01-12 20:41:02 +0000

    Added basic GameRenderer stubs for rendering a list of Sprite and Text objects.
    git-svn-id: svn+ssh://leetcode.net/home/jreese/backup/srv/svn/repos/rit/2dgames/trunk@450 ad4848e8-a12d-0410-9a8f-f544a64f585b

--- SnailsPace/SnailsPace/GameRenderer.cs
+++ SnailsPace/SnailsPace/GameRenderer.cs
@@ -1,10 +1,23 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Xna.Framework;
 
 namespace A_Snail_s_Pace
 {
     class GameRenderer
     {
+        // Camera information
+        private Vector3 cameraPosition;
+        private Vector3 cameraTargetPosition;
+        private Vector3 cameraView;
+
+        public GameRenderer()
+        {
+        }
+
+        public void render(List<Objects.Sprite> sprites, List<Objects.Text> strings)
+        {
+        }
     }
 }
