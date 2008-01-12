b2cdf9540abfb1b56b5fe69dd5c48cf23b1fbed8
jreese <jreese@ad4848e8-a12d-0410-9a8f-f544a64f585b>
2008-01-12 20:40:10 +0000

    Added Objects/Text.cs class to handle string informations.
    git-svn-id: svn+ssh://leetcode.net/home/jreese/backup/srv/svn/repos/rit/2dgames/trunk@449 ad4848e8-a12d-0410-9a8f-f544a64f585b

---
+++ SnailsPace/SnailsPace/Objects/Text.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace A_Snail_s_Pace.Objects
+{
+    class Text
+    {
+        // Text position and such
+        public Vector3 position;
+        public Vector3 rotation;
+
+        // Text properties
+        public String content;
+        public SpriteFont font;
+        public Color color;
+    }
+}
