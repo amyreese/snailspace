321f0c983134527dd8f2fd796291bdd4c214c63a
jreese <jreese@ad4848e8-a12d-0410-9a8f-f544a64f585b>
2008-01-13 04:35:12 +0000

    !FINALLY!  Fixed Lua not loading our C# code.
    The problem was that when loading assemblies, it doesn't use the *namespace*, but the AssemblyTitle as defined by Properties/AssemblyInfo.cs, something I didn't figure out until I went through the LuaInterface source code.  Phew!
    
    git-svn-id: svn+ssh://leetcode.net/home/jreese/backup/srv/svn/repos/rit/2dgames/trunk@473 ad4848e8-a12d-0410-9a8f-f544a64f585b

--- SnailsPace/SnailsPace/Properties/AssemblyInfo.cs
+++ SnailsPace/SnailsPace/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("A_Snail_s_Pace")]
-[assembly: AssemblyProduct("A_Snail_s_Pace")]
+[assembly: AssemblyTitle("SnailsPace")]
+[assembly: AssemblyProduct("A Snail's Pace")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("")]
 
-[assembly: AssemblyCopyright("Copyright ©  2007")]
+[assembly: AssemblyCopyright("Copyright ©  2008")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
