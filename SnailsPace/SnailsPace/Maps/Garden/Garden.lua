43f6acf03c6eab7b8e7cbf3ce9fe63b276f8cbea
pdobson <pdobson@ad4848e8-a12d-0410-9a8f-f544a64f585b>
2008-01-19 20:16:23 +0000

    git-svn-id: svn+ssh://leetcode.net/home/jreese/backup/srv/svn/repos/rit/2dgames/trunk@505 ad4848e8-a12d-0410-9a8f-f544a64f585b

--- SnailsPace/SnailsPace/Maps/Garden/Garden.lua
+++ SnailsPace/SnailsPace/Maps/Garden/Garden.lua
@@ -21,3 +21,22 @@ go.sprites:Add("Snail", sprite)
 go.velocity = Vector2(3.0, 2.0)
 
 map.objects:Add(go)
+
+img = Image()
+img.filename = "Resources/Textures/BlackAnt_1"
+img.blocks = Vector2(1.0, 1.0)
+img.size = Vector2(500.0, 500.0)
+
+sprt = Sprite()
+sprt.image = img
+sprt.visible = true
+sprt.effect = "Resources/Effects/effects"
+sprt.animationStart = 0
+sprt.animationEnd = 0
+sprt.frame = 0
+
+gmob = GameObject()
+gmob.sprites:Add("Ant", sprt)
+gmob.velocity = Vector2(3.0, 2.0)
+
+map.objects:Add(gmob)
