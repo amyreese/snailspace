04e1d6c895a16c6d07b6058eed1ec1c19dcb8ba6
bschroth <bschroth@ad4848e8-a12d-0410-9a8f-f544a64f585b>
2008-01-23 19:56:48 +0000

    Added in listing of game controls
    git-svn-id: svn+ssh://leetcode.net/home/jreese/backup/srv/svn/repos/rit/2dgames/trunk@537 ad4848e8-a12d-0410-9a8f-f544a64f585b

--- SnailsPace/SnailsPace/Screens/Menus/SettingsMenuScreen.cs
+++ SnailsPace/SnailsPace/Screens/Menus/SettingsMenuScreen.cs
@@ -17,14 +17,22 @@ namespace SnailsPace.Screens.Menus
             : base(game)
         {
         }
-
+		
         protected override void SetupMenuItems()
         {
+			Input input = SnailsPace.inputManager;
+
             float itemY = spriteFont.LineSpacing;
             float itemX = 25.0f;
-            menuItems = new MenuItem[1];
+            menuItems = new MenuItem[7];
             menuItems[0] = new MenuItem("Back", this, new Vector2(itemX, itemY));
-            menuItemIndex = 0;
+			menuItems[1] = new MenuItem("Move Up/Jetpack: " + input.getKeyBinding("Up"), this, new Vector2(itemX, itemY + spriteFont.LineSpacing * 2));
+			menuItems[2] = new MenuItem("Move Left: " +input.getKeyBinding("Left"), this, new Vector2(itemX, itemY + spriteFont.LineSpacing * 4));
+			menuItems[3] = new MenuItem("Move Down: " + input.getKeyBinding("Down"), this, new Vector2(itemX, itemY + spriteFont.LineSpacing * 6));
+			menuItems[4] = new MenuItem("Move Right: " + input.getKeyBinding("Right"), this, new Vector2(itemX, itemY + spriteFont.LineSpacing * 8));
+			menuItems[5] = new MenuItem("Fire: " + input.getKeyBinding("Fire"), this, new Vector2(itemX, itemY + spriteFont.LineSpacing * 10));
+			menuItems[6] = new MenuItem("Pause: " + input.getKeyBinding("Pause"), this, new Vector2(itemX, itemY + spriteFont.LineSpacing * 12));
+			menuItemIndex = 0;
             ready = true;
         }
 
