384ac7d62bfe4c0e553f0249e9bb4fe76dfe9ddf
bschroth <bschroth@ad4848e8-a12d-0410-9a8f-f544a64f585b>
2008-02-09 23:14:53 +0000

    Refactored parts of MoveOrCollide by extracted into separate methods.
    git-svn-id: svn+ssh://leetcode.net/home/jreese/backup/srv/svn/repos/rit/2dgames/trunk@699 ad4848e8-a12d-0410-9a8f-f544a64f585b

--- SnailsPace/SnailsPace/Core/Engine.cs
+++ SnailsPace/SnailsPace/Core/Engine.cs
@@ -47,6 +47,9 @@ namespace SnailsPace.Core
 		// Invisible map bounding objects
 		public List<Objects.GameObject> mapBounds;
 
+		// list of objects that are colliding in this frame
+		private List<Objects.GameObject> collidingObjects;
+
 		// HUD sprites
 		public static Texture2D healthBar;
 		public static Texture2D healthIcon;
@@ -63,6 +66,7 @@ namespace SnailsPace.Core
 			map = new Objects.Map(mapName);
 
 			bullets = new List<Objects.Bullet>();
+			collidingObjects = new List<Objects.GameObject>();
 
 			loadFonts();
 			loadHUD();
@@ -165,15 +169,15 @@ namespace SnailsPace.Core
 			gameRenderer.cameraBounds = map.bounds.ToArray();
 		}
 
-        bool tada = false;
+		bool tada = false;
 		public void think(GameTime gameTime)
 		{
 			Engine.gameTime = gameTime;
 			Input input = SnailsPace.inputManager;
 
-            if (input.inputPressed("Pause"))
+			if (input.inputPressed("Pause"))
 			{
-                SnailsPace.soundManager.playRepeat("tada");
+				SnailsPace.soundManager.playRepeat("tada");
 				enginePaused = !enginePaused;
 			}
 			if (input.inputPressed("MenuToggle"))
@@ -196,62 +200,62 @@ namespace SnailsPace.Core
 			while (charEnum.MoveNext())
 			{
 				//Is a character dead? Kill it!
-                if (charEnum.Current.health <= 0)
-                {
-                    deadChars.Add(charEnum.Current);
-                    player.killedEnemy();
-                }
-                //If not, let it think.
-                else
-                {
-                    charEnum.Current.think(gameTime);
-                }
+				if (charEnum.Current.health <= 0)
+				{
+					deadChars.Add(charEnum.Current);
+					player.killedEnemy();
+				}
+				//If not, let it think.
+				else
+				{
+					charEnum.Current.think(gameTime);
+				}
 			}
 			charEnum.Dispose();
 
 			List<Objects.Character>.Enumerator deadCharEnum = deadChars.GetEnumerator();
-            while (deadCharEnum.MoveNext())
-            {
-                map.characters.Remove(deadCharEnum.Current);
-            }
+			while (deadCharEnum.MoveNext())
+			{
+				map.characters.Remove(deadCharEnum.Current);
+			}
 			deadCharEnum.Dispose();
 
 #if DEBUG
 			if (input.inputPressed("DebugFramerate"))
 			{
 				SnailsPace.debugFramerate = !SnailsPace.debugFramerate;
-                Console.WriteLine("Debug Framerate: " + SnailsPace.debugFramerate);
-            }
+				Console.WriteLine("Debug Framerate: " + SnailsPace.debugFramerate);
+			}
 			if (input.inputPressed("DebugCollisions"))
 			{
 				SnailsPace.debugCollisions = !SnailsPace.debugCollisions;
-                Console.WriteLine("Debug Collisions: " + SnailsPace.debugCollisions);
-            }
+				Console.WriteLine("Debug Collisions: " + SnailsPace.debugCollisions);
+			}
 			if (input.inputPressed("DebugCulling"))
 			{
 				SnailsPace.debugCulling = !SnailsPace.debugCulling;
-                Console.WriteLine("Debug Culling: " + SnailsPace.debugCulling);
-            }
+				Console.WriteLine("Debug Culling: " + SnailsPace.debugCulling);
+			}
 			if (input.inputPressed("DebugBoundingBoxes"))
 			{
 				SnailsPace.debugBoundingBoxes = !SnailsPace.debugBoundingBoxes;
-                Console.WriteLine("Debug Bounding Boxes: " + SnailsPace.debugBoundingBoxes);
-            }
+				Console.WriteLine("Debug Bounding Boxes: " + SnailsPace.debugBoundingBoxes);
+			}
 			if (input.inputPressed("DebugFlying"))
 			{
 				SnailsPace.debugFlying = !SnailsPace.debugFlying;
-                Console.WriteLine("Debug Flying: " + SnailsPace.debugFlying);
-            }
+				Console.WriteLine("Debug Flying: " + SnailsPace.debugFlying);
+			}
 			if (input.inputPressed("DebugCameraPosition"))
 			{
 				SnailsPace.debugCameraPosition = !SnailsPace.debugCameraPosition;
-                Console.WriteLine("Debug Camera Position: " + SnailsPace.debugCameraPosition);
-            }
+				Console.WriteLine("Debug Camera Position: " + SnailsPace.debugCameraPosition);
+			}
 			if (input.inputPressed("DebugHelixPosition"))
 			{
 				SnailsPace.debugHelixPosition = !SnailsPace.debugHelixPosition;
-                Console.WriteLine("Debug Helix Position: " + SnailsPace.debugHelixPosition);
-            }
+				Console.WriteLine("Debug Helix Position: " + SnailsPace.debugHelixPosition);
+			}
 			if (input.inputDown("DebugZoomIn"))
 			{
 				Renderer.debugZoom -= 0.25f;
@@ -265,11 +269,11 @@ namespace SnailsPace.Core
 			{
 				Renderer.debugZoom += 0.25f;
 				Renderer.farClip = 500.0f + 2 * Renderer.normalCameraDistance * Renderer.debugZoom;
-            }
+			}
 			if (input.inputPressed("DebugTriggers"))
 			{
 				SnailsPace.debugTriggers = !SnailsPace.debugTriggers;
-                Console.WriteLine("Debug Triggers: " + SnailsPace.debugTriggers);
+				Console.WriteLine("Debug Triggers: " + SnailsPace.debugTriggers);
 			}
 #endif
 
@@ -348,152 +352,81 @@ namespace SnailsPace.Core
 		}
 
 		private readonly Vector2 gravity = new Vector2(0.0f, -512.0f);
+
+		private bool IsWithinBounds(Objects.GameObject movingObject, Vector2 objectMovement, Vector2 boundsSize, Vector2 boundsCenter)
+		{
+			if (movingObject.position.X < boundsCenter.X)
+			{
+				if (objectMovement.X < 0)
+				{
+					float left = movingObject.position.X - movingObject.size.X / 2;
+					float leftDiff = left - boundsCenter.X;
+					float leftDiffAbs = Math.Abs(leftDiff);
+					if (leftDiffAbs > boundsSize.X / 2)
+					{
+						return false;
+					}
+				}
+			}
+			else
+			{
+				if (objectMovement.X > 0)
+				{
+					float right = movingObject.position.X + movingObject.size.X / 2;
+					float rightDiff = right - boundsCenter.X;
+					float rightDiffAbs = Math.Abs(rightDiff);
+					if (rightDiffAbs > boundsSize.X / 2)
+					{
+						return false;
+					}
+				}
+			}
+			if (movingObject.position.Y < boundsCenter.Y)
+			{
+				if (objectMovement.Y < 0)
+				{
+					float bottom = movingObject.position.Y - movingObject.size.Y / 2;
+					float bottomDiff = bottom - boundsCenter.Y;
+					float bottomDiffAbs = Math.Abs(bottomDiff);
+					if (bottomDiffAbs > boundsSize.Y / 2)
+					{
+						return false;
+					}
+				}
+			}
+			else
+			{
+				if (objectMovement.Y > 0)
+				{
+					float top = movingObject.position.Y + movingObject.size.Y / 2;
+					float topDiff = top - boundsCenter.Y;
+					float topDiffAbs = Math.Abs(topDiff);
+					if (topDiffAbs > boundsSize.Y / 2)
+					{
+						return false;
+					}
+				}
+			} 
+			return true;
+		}
 		private void MoveOrCollide(Objects.GameObject movingObject, List<Objects.GameObject> collidableObjects, float elapsedTime, Vector2 boundsSize, Vector2 boundsCenter)
 		{
 			if ((gravityEnabled && movingObject.affectedByGravity) || movingObject.velocity.Length() > 0 || movingObject.direction.Length() > 0)
 			{
-				Vector2 objectVelocity = Vector2.Zero;
-				Vector2 objectAccel = Vector2.Zero;
-				// Calculate their velocity after acceleration
-				if (movingObject.direction.Length() > 0)
-				{
-					movingObject.direction.Normalize();
-					if (movingObject.desiredMaxVelocity == 0)
-					{
-						movingObject.desiredMaxVelocity = movingObject.maxVelocity;
-					}
-					objectAccel = movingObject.direction * movingObject.acceleration * elapsedTime;
-					objectVelocity = objectAccel + movingObject.velocity;
-					Vector2 objectVelocityLessGravity = objectVelocity - movingObject.velocityFromGravity;
-					if (objectVelocityLessGravity.Y > movingObject.maxVelocity - movingObject.terminalVelocity)
-					{
-						if (objectVelocity.Length() > movingObject.desiredMaxVelocity)
-						{
-							objectVelocity.Normalize();
-							objectVelocity = objectVelocity * movingObject.desiredMaxVelocity;
-						}
-					}
-					else
-					{
-						if (objectVelocityLessGravity.Length() > movingObject.desiredMaxVelocity)
-						{
-							objectVelocityLessGravity.Normalize();
-							objectVelocityLessGravity = objectVelocityLessGravity * movingObject.desiredMaxVelocity;
-							objectVelocity = objectVelocityLessGravity + movingObject.velocityFromGravity;
-						}
-					}
-				}
-				else
-				{
-					objectVelocity = movingObject.velocity;
-					if (objectVelocity.X == 0)
-					{
-						// stay at 0
-					}
-					else if (objectVelocity.X > 0)
-					{
-						objectVelocity.X -= movingObject.horizontalFriction * elapsedTime;
-						if (objectVelocity.X < 0)
-						{
-							objectVelocity.X = 0;
-						}
-					}
-					else
-					{
-						objectVelocity.X += movingObject.horizontalFriction * elapsedTime;
-						if (objectVelocity.X > 0)
-						{
-							objectVelocity.X = 0;
-						}
-					}
-				}
-
-				// Calculate their velocity after gravity;
-				if (gravityEnabled && movingObject.affectedByGravity)
-				{
-					if (-objectVelocity.Y < movingObject.terminalVelocity)
-					{
-						Vector2 velocityFromGravity = gravity * elapsedTime;
-						objectVelocity += velocityFromGravity;
-						movingObject.velocityFromGravity += velocityFromGravity;
-						if (-objectVelocity.Y > movingObject.terminalVelocity)
-						{
-							objectVelocity.Y = -movingObject.terminalVelocity;
-							movingObject.velocityFromGravity = new Vector2(0, -movingObject.terminalVelocity);
-						}
-					}
-				}
+				
+				Vector2 objectVelocity = GetObjectVelocity(movingObject, elapsedTime);
 				Vector2 objectMovement = Vector2.Multiply(objectVelocity, elapsedTime);
 				Vector2 resultingMovement = Vector2.Zero;
 
-				if (movingObject.position.X < boundsCenter.X)
+				if (!IsWithinBounds( movingObject, objectMovement, boundsSize, boundsCenter ))
 				{
-					if (objectMovement.X < 0)
+					if (movingObject is Objects.Bullet)
 					{
-						float left = movingObject.position.X - movingObject.size.X / 2;
-						float leftDiff = left - boundsCenter.X;
-						float leftDiffAbs = Math.Abs(leftDiff);
-						if (leftDiffAbs > boundsSize.X / 2)
-						{
-							if (movingObject is Objects.Bullet)
-							{
-								bulletsToClear.Add((Objects.Bullet)movingObject);
-							}
-							return;
-						}
+						bulletsToClear.Add((Objects.Bullet)movingObject);
 					}
+					return;
 				}
-				else
-				{
-					if (objectMovement.X > 0)
-					{
-						float right = movingObject.position.X + movingObject.size.X / 2;
-						float rightDiff = right - boundsCenter.X;
-						float rightDiffAbs = Math.Abs(rightDiff);
-						if (rightDiffAbs > boundsSize.X / 2)
-						{
-							if (movingObject is Objects.Bullet)
-							{
-								bulletsToClear.Add((Objects.Bullet)movingObject);
-							}
-							return;
-						}
-					}
-				}
-				if (movingObject.position.Y < boundsCenter.Y)
-				{
-					if (objectMovement.Y < 0)
-					{
-						float bottom = movingObject.position.Y - movingObject.size.Y / 2;
-						float bottomDiff = bottom - boundsCenter.Y;
-						float bottomDiffAbs = Math.Abs(bottomDiff);
-						if (bottomDiffAbs > boundsSize.Y / 2)
-						{
-							if (movingObject is Objects.Bullet)
-							{
-								bulletsToClear.Add((Objects.Bullet)movingObject);
-							}
-							return;
-						}
-					}
-				}
-				else
-				{
-					if (objectMovement.Y > 0)
-					{
-						float top = movingObject.position.Y + movingObject.size.Y / 2;
-						float topDiff = top - boundsCenter.Y;
-						float topDiffAbs = Math.Abs(topDiff);
-						if (topDiffAbs > boundsSize.Y / 2)
-						{
-							if (movingObject is Objects.Bullet)
-							{
-								bulletsToClear.Add((Objects.Bullet)movingObject);
-							}
-							return;
-						}
-					}
-				}
+				
 
 				List<Objects.GameObject> remainingCollidableObjects;
 				Objects.GameObject collidedObject = CheckForCollision(movingObject, collidableObjects, objectMovement, out remainingCollidableObjects);
@@ -576,6 +509,82 @@ namespace SnailsPace.Core
 			}
 		}
 
+		private Vector2 GetObjectVelocity(Objects.GameObject movingObject, float elapsedTime)
+		{
+			Vector2 objectVelocity = Vector2.Zero;
+			Vector2 objectAccel = Vector2.Zero;
+			// Calculate their velocity after acceleration
+			if (movingObject.direction.Length() > 0)
+			{
+				movingObject.direction.Normalize();
+				if (movingObject.desiredMaxVelocity == 0)
+				{
+					movingObject.desiredMaxVelocity = movingObject.maxVelocity;
+				}
+				objectAccel = movingObject.direction * movingObject.acceleration * elapsedTime;
+				objectVelocity = objectAccel + movingObject.velocity;
+				Vector2 objectVelocityLessGravity = objectVelocity - movingObject.velocityFromGravity;
+				if (objectVelocityLessGravity.Y > movingObject.maxVelocity - movingObject.terminalVelocity)
+				{
+					if (objectVelocity.Length() > movingObject.desiredMaxVelocity)
+					{
+						objectVelocity.Normalize();
+						objectVelocity = objectVelocity * movingObject.desiredMaxVelocity;
+					}
+				}
+				else
+				{
+					if (objectVelocityLessGravity.Length() > movingObject.desiredMaxVelocity)
+					{
+						objectVelocityLessGravity.Normalize();
+						objectVelocityLessGravity = objectVelocityLessGravity * movingObject.desiredMaxVelocity;
+						objectVelocity = objectVelocityLessGravity + movingObject.velocityFromGravity;
+					}
+				}
+			}
+			else
+			{
+				objectVelocity = movingObject.velocity;
+				if (objectVelocity.X == 0)
+				{
+					// stay at 0
+				}
+				else if (objectVelocity.X > 0)
+				{
+					objectVelocity.X -= movingObject.horizontalFriction * elapsedTime;
+					if (objectVelocity.X < 0)
+					{
+						objectVelocity.X = 0;
+					}
+				}
+				else
+				{
+					objectVelocity.X += movingObject.horizontalFriction * elapsedTime;
+					if (objectVelocity.X > 0)
+					{
+						objectVelocity.X = 0;
+					}
+				}
+			}
+
+			// Calculate their velocity after gravity;
+			if (gravityEnabled && movingObject.affectedByGravity)
+			{
+				if (-objectVelocity.Y < movingObject.terminalVelocity)
+				{
+					Vector2 velocityFromGravity = gravity * elapsedTime;
+					objectVelocity += velocityFromGravity;
+					movingObject.velocityFromGravity += velocityFromGravity;
+					if (-objectVelocity.Y > movingObject.terminalVelocity)
+					{
+						objectVelocity.Y = -movingObject.terminalVelocity;
+						movingObject.velocityFromGravity = new Vector2(0, -movingObject.terminalVelocity);
+					}
+				}
+			}
+			return objectVelocity;
+		}
+
 		public void physics(GameTime gameTime)
 		{
 			if (enginePaused)
@@ -628,12 +637,14 @@ namespace SnailsPace.Core
 				bool noQuadTree = true;
 				if (!noQuadTree)
 				{
-					Rectangle visibleScreen = new Rectangle((int)(Player.helix.position.X - 600), (int)(Player.helix.position.Y) - 600, 1200, 1200);
+					Rectangle visibleScreen = new Rectangle((int)(Player.helix.position.X - 600), (int)(Player.helix.position.Y) - 600, 1400, 1400);
 					QuadTree quad = new QuadTree(collidableObjects, visibleScreen, 2);
-#if DEBUG
-					//Debug.WriteLine( helix.name + ": (" + helix.position.X + "," + helix.position.Y + ")" );
-					//quad.print();
-#endif
+
+					List<Objects.GameObject>.Enumerator collidingObjectEnum = collidingObjects.GetEnumerator();
+					while (collidingObjectEnum.MoveNext())
+					{
+						MoveOrCollide(collidingObjectEnum.Current, collidingObjects, elapsedTime, boundsSize, boundsCenter);
+					}
 					detectCollisionsInNode(quad.getRoot(), elapsedTime, boundsSize, boundsCenter);
 				}
 
@@ -658,28 +669,28 @@ namespace SnailsPace.Core
 			}
 
 			// Check all triggers against Helix
-            List<Objects.Trigger>.Enumerator triggers = map.triggers.GetEnumerator();
+			List<Objects.Trigger>.Enumerator triggers = map.triggers.GetEnumerator();
 			while (triggers.MoveNext())
 			{
 				Objects.Trigger trigger = triggers.Current;
 
-                if (trigger.bounds.containsPoint(Player.helix.position.X, Player.helix.position.Y))
-                {
-                    if (!trigger.inside)
-                    {
-                        trigger.triggerIn(Player.helix);
-                    }
-                    trigger.trigger(Player.helix);
-                }
-                else
-                {
-                    if (trigger.inside)
-                    {
-                        trigger.triggerOut(Player.helix);
-                    }
-                }
-            }
-            triggers.Dispose();
+				if (trigger.bounds.containsPoint(Player.helix.position.X, Player.helix.position.Y))
+				{
+					if (!trigger.inside)
+					{
+						trigger.triggerIn(Player.helix);
+					}
+					trigger.trigger(Player.helix);
+				}
+				else
+				{
+					if (trigger.inside)
+					{
+						trigger.triggerOut(Player.helix);
+					}
+				}
+			}
+			triggers.Dispose();
 
 			// Animate everything
 			{
@@ -728,8 +739,13 @@ namespace SnailsPace.Core
 				List<Objects.GameObject>.Enumerator objectEnumerator = containedObjects.GetEnumerator();
 				while (objectEnumerator.MoveNext())
 				{
-					String objectName = node.name;
-					MoveOrCollide(objectEnumerator.Current, containedObjects, elapsedTime, boundsSize, boundsCenter);
+					String nodeName = node.name;
+					Vector2 objectVelocity = GetObjectVelocity(objectEnumerator.Current, elapsedTime);
+					Vector2 objectMovement = Vector2.Multiply(objectVelocity, elapsedTime);
+					if (CheckForCollision(objectEnumerator.Current, containedObjects, objectMovement) != null)
+					{
+						collidingObjects.Add(objectEnumerator.Current);
+					}
 				}
 			}
 			else
@@ -813,25 +829,25 @@ namespace SnailsPace.Core
 		{
 			List<Objects.GameObject> objects = new List<Objects.GameObject>(map.objects);
 
-            objects.AddRange(mapBounds);
+			objects.AddRange(mapBounds);
 
-            List<Objects.Character>.Enumerator characterEnum = map.characters.GetEnumerator();
-            while (characterEnum.MoveNext())
-            {
-                objects.Add(characterEnum.Current);
-            }
-            characterEnum.Dispose();
+			List<Objects.Character>.Enumerator characterEnum = map.characters.GetEnumerator();
+			while (characterEnum.MoveNext())
+			{
+				objects.Add(characterEnum.Current);
+			}
+			characterEnum.Dispose();
 
-            objects.AddRange(player.gameObjects());
+			objects.AddRange(player.gameObjects());
 
-            List<Objects.Bullet>.Enumerator bulletEnum = bullets.GetEnumerator();
-            while (bulletEnum.MoveNext())
-            {
-                objects.Add(bulletEnum.Current);
-            }
-            bulletEnum.Dispose();
+			List<Objects.Bullet>.Enumerator bulletEnum = bullets.GetEnumerator();
+			while (bulletEnum.MoveNext())
+			{
+				objects.Add(bulletEnum.Current);
+			}
+			bulletEnum.Dispose();
 
-            objects.Add(pause);
+			objects.Add(pause);
 
 			return objects;
 		}
