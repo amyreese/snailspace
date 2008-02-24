6e8186fddeea7b91f437a0da0e0d9ed8a7f9f871
jreese <jreese@ad4848e8-a12d-0410-9a8f-f544a64f585b>
2008-02-24 21:24:25 +0000

    Added rock sprite, beginning modification and enhancements to credits level.
    git-svn-id: svn+ssh://leetcode.net/home/jreese/backup/srv/svn/repos/rit/2dgames/trunk@894 ad4848e8-a12d-0410-9a8f-f544a64f585b

--- SnailsPace/SnailsPace/Maps/Credits/Credits.lua
+++ SnailsPace/SnailsPace/Maps/Credits/Credits.lua
@@ -3,8 +3,11 @@ library('WorldBuilding')
 
 -- Player creation and starting position
 startPosition = Vector2(0,0)
-startPosition = Vector2(8600,0)
 player = Player( startPosition, "generic" )
+player.helix:AddWeapon(Weapon.load("stinger"))
+player.helix:AddWeapon(Weapon.load("minigun"))
+player.helix:AddWeapon(Weapon.load("flamethrower"))
+player.helix:AddWeapon(Weapon.load("grenadelauncher"))
 
 rotmod = 5
 function newRotation()
@@ -32,6 +35,37 @@ function LoadArea( name )
 end
 
 LoadArea("Credits")
+
+rock = GameObject()
+rsprite = Sprite()
+rsprite.image = Image()
+rsprite.image.filename = "Resources/Textures/Rock"
+rsprite.image.blocks = Vector2(1,1)
+rsprite.image.size = Vector2(512,512)
+rsprite.effect = "Resources/Effects/effects"
+rsprite.visible = true
+rock.sprites:Add("Rock", rsprite)
+
+rbounds = GameObjectBoundsBuilder()
+rbounds:AddPoint(Vector2(-190,-40))
+rbounds:AddPoint(Vector2(-170,50))
+rbounds:AddPoint(Vector2(-100,120))
+rbounds:AddPoint(Vector2(-50,160))
+rbounds:AddPoint(Vector2(45,170))
+rbounds:AddPoint(Vector2(170,100))
+rbounds:AddPoint(Vector2(200,40))
+rbounds:AddPoint(Vector2(210,-20))
+rbounds:AddPoint(Vector2(160,-60))
+rbounds:AddPoint(Vector2(0,-80))
+rbounds:AddPoint(Vector2(-170,-60))
+rock.bounds = rbounds:BuildBounds()
+
+rock.position = Vector2( 760, 0 )
+rock.size = rsprite.image.size
+rock.affectedByGravity = false
+rock.collidable = true	
+
+map.objects:Add(rock)
 
 -- Set the bounds for this map
 map.bounds:Add(Vector2(-512, -196))
